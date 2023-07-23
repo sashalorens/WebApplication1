6c03f5bc17a7887d7b00e4749171590dddd89bbd
slorens <js.slorens@gmail.com>
2023-07-23 20:19:59 +0300

    Implementing routing middleware

---
+++ CarManagement.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication1
+{
+    public class CarManagement: ICarManagement
+    {
+        public async Task GetCarName(string name, HttpContext context)
+        {
+            await context.Response.WriteAsync($"The car name is {name} \n");
+        }
+
+        public async Task GetCarEngine(string engine, HttpContext context)
+        {
+            await context.Response.WriteAsync($"The car has {engine} engine \n");
+        }
+
+        public async Task GetCarAge(string age, HttpContext context)
+        {
+            await context.Response.WriteAsync($"The age of car is {age} years \n");
+        }
+    }
+}
---
+++ ICarManagement.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1
+{
+    public interface ICarManagement
+    {
+        public Task GetCarName(string name, HttpContext context);
+
+        public Task GetCarEngine(string engine, HttpContext context);
+
+        public Task GetCarAge(string age, HttpContext context);
+    }
+}
